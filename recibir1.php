20db5a6efb6d97b8f3015082f716164be5acfa3b
dayanavaleria654-boop <dayanavaleria654@gmail.com>
2026-05-20 09:33:48 -0600

    ejercicio_1
    Elabore un formulario que solicite el nombre del usuario y envíe la información a
    otro archivo PHP donde se muestre un mensaje de bienvenida con el nombre
    ingresado.

---
+++ recibir1.php
@@ -0,0 +1,14 @@
+<?php
+
+$nombre = $_POST['nombre'];
+
+echo "<body bgcolor='pink'>";
+echo "<center>";
+
+echo "<h2>¡Bienvenido!</h2>";
+
+echo "<b>Felicidades, has ingresado tu nombre:</b> " . $nombre;
+
+echo "</center>";
+
+?>
